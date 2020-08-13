5312eea3bc8af19082b3f6f979daf36f1c9799a6
Keremcan Buyuktaskin <keremcan@gmail.com>
2020-08-14 01:50:04 +0300

    signup

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer';
+import { Context as AuthContext } from '../context/AuthContext';
 
 const SignupScreen = ({ navigation }) => {
+  const { state, signup } = useContext(AuthContext);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -29,15 +31,20 @@ const SignupScreen = ({ navigation }) => {
         autoCorrect={false}
         secureTextEntry
       />
+      {!!state.errorMessage && (
+        <Text style={styles.errorMessage}>{state.errorMessage}</Text>
+      )}
       <Spacer>
-        <Button title='Sign Up' />
+        <Button title='Sign Up' onPress={() => signup({ email, password })} />
       </Spacer>
     </View>
   );
 };
 
 SignupScreen.navigationOptions = () => {
-  headerShown: false;
+  return {
+    headerShown: false,
+  };
 };
 
 const styles = StyleSheet.create({
@@ -45,6 +52,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     marginBottom: 250,
+  },
+  errorMessage: {
+    fontSize: 16,
+    color: 'red',
+    marginLeft: 15,
+    marginTop: 15,
   },
 });
 
