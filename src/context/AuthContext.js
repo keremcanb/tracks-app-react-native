c56e71a847a504ac711b1d0859a69d66a45c3227
keremcanb <keremcan@gmail.com>
2020-08-15 22:30:51 +0300

    Signout

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -11,6 +11,8 @@ const authReducer = (state, action) => {
       return { ...state, errorMessage: action.payload };
     case 'clear_error_message':
       return { ...state, errorMessage: '' };
+    case 'signout':
+      return { token: null, errorMessage: '' };
     default:
       return state;
   }
@@ -64,7 +66,11 @@ const signin = (dispatch) => async ({ email, password }) => {
   }
 };
 
-const signout = (dispatch) => {};
+const signout = (dispatch) => async () => {
+  await AsyncStorage.removeItem('token');
+  dispatch({ type: 'signout' });
+  navigate('loginFlow');
+};
 
 export const { Provider, Context } = createDataContext(
   authReducer,
